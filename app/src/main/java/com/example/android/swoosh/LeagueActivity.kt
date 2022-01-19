499b4e435c2bcb08124336238a8c2d9bb3733815
Ritik Rawat <75121026+ritikrawat2447@users.noreply.github.com>
2022-01-20 00:59:25 +0530

    lesson-04

--- app/src/main/java/com/example/android/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/android/swoosh/LeagueActivity.kt
@@ -1,11 +1,18 @@
 package com.example.android.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun LeagueNxtClicked(View : View){
+        val skilledActivity = Intent(this,SkillActivity::class.java)
+        startActivity(skilledActivity)
     }
 }
--- app/src/main/java/com/example/android/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/android/swoosh/MainActivity.kt
@@ -3,21 +3,20 @@ package com.example.android.swoosh
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.android.swoosh.databinding.ActivityMainBinding
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
-    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
         setContentView(R.layout.activity_main)
 
-        binding.getStartedBtn.setOnClickListener {
+        val btn : Button = findViewById(R.id.getStartedBtn)
+        btn.setOnClickListener {
             val leagueIntent = Intent(this,LeagueActivity::class.java)
             startActivity(leagueIntent)
+
         }
     }
 
